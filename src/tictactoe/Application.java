92f71c7c5136c4a5d315dabd232bcbd4d08e57f5
Lukas Angerer <lukas.angerer@getunik.com>
2013-11-01 14:44:23 +0100

    initial commit of tic-tac-toe sample project

---
+++ src/tictactoe/Application.java
@@ -0,0 +1,50 @@
+package tictactoe;
+
+import java.io.*;
+
+public class Application
+{
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Application a = new Application();
+		a.run();
+	}
+	
+	private BufferedReader _reader;
+	
+	public Application()
+	{
+		_reader = new BufferedReader(new InputStreamReader(System.in));
+	}
+	
+	private void run()
+	{
+		writeLine("Welcome to Tic-Tac-Toe");
+		writeLine("Enter 'q' or 'quit' to exit");
+		writeLine("Hit RETURN to quit");
+		String line = readLine();
+		writeLine(line);
+	}
+
+	private void writeLine(String line)
+	{
+		System.out.println(line);
+	}
+	
+	private String readLine()
+	{
+		try
+		{
+			String line = _reader.readLine();
+			return line;
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
